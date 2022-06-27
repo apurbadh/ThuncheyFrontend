d35bc074bd5f68565687aa1ca39ad0bfb2b704e8
Apurba Adhikari <erikduke182@gmail.com>
2022-06-27 14:21:30 +0545

    Routes managed in App.jsx file and the Home component set as the component for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
-        
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   )
