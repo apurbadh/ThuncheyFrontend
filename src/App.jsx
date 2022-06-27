f2ca98780b87d249572d4e138b66eef18b0870d8
Apurba Adhikari <erikduke182@gmail.com>
2022-06-27 23:11:27 +0545

    Folders and CSS for pages and components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import { useState, useEffect } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 import { getUser } from "./scripts/user"
 
 function App() {
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Spinner } from '@chakra-ui/react'
+import "./Home.css"
+
+function Home() {
+    return <div>
+        <Spinner className='spinner' size='xl'/>
+    </div>
+}
+
+export default Home;
